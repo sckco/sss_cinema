41b3020f07d6bceae7d76a76819246e8b1b46b53
sckco <danielnobokiz@gmail.com>
2025-11-30 00:28:33 +0700

    edit home lagi

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -16,34 +16,34 @@ class HomeScreen extends StatelessWidget {
         elevation: 0,
         backgroundColor: Colors.transparent,
         title: const Text(
-        "SSS Cinema",
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontSize: 26,
-          letterSpacing: 1.2,
-          color: Colors.white,
+          "SSS Cinema",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 26,
+            letterSpacing: 1.2,
+            color: Colors.white,
+          ),
         ),
+        centerTitle: true,
       ),
-      centerTitle: true,
-    ),
       body: RefreshIndicator(
         onRefresh: () => providerDaniel.loadMoviesDaniel(),
-        child: providerDaniel.loadingDaniel
-            ? const Center(child: CircularProgressIndicator())
-            : GridView.builder(
-                padding: const EdgeInsets.all(12),
-                itemCount: providerDaniel.movieListDaniel.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 12,
-                  childAspectRatio: 0.62,
-                ),
-                itemBuilder: (context, index) {
-                  MovieModel movieDaniel = providerDaniel.movieListDaniel[index];
-                  return MovieItemWidget(movieDaniel: movieDaniel);
-                },
-              ),
+        color: Colors.white,
+        backgroundColor: Colors.black,
+        child: GridView.builder(
+          padding: const EdgeInsets.fromLTRB(16, 100, 16, 16),
+          itemCount: providerDaniel.movieListDaniel.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 18,
+            crossAxisSpacing: 18,
+            childAspectRatio: 0.62,
+          ),
+          itemBuilder: (context, index) {
+            final movieDaniel = providerDaniel.movieListDaniel[index];
+            return MovieItemWidget(movieDaniel: movieDaniel);
+          },
+        ),
       ),
     );
   }
