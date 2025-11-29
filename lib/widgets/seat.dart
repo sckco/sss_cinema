960b09ee7e8cb1bec9b86f3925653fde6263fe13
alfinnaza <alfinkirom20@gmail.com>
2025-11-30 00:31:07 +0700

    seat revisi 1

--- lib/widgets/seat.dart
+++ lib/widgets/seat.dart
@@ -10,4 +10,25 @@ class SeatItemNaza extends StatelessWidget {
 
   const SeatItemNaza({required this.seatId, required this.isSold, required this.isSelected, required this.onTap});
 
-  
+  @override
+  Widget build(BuildContext context) {
+    Color color;
+    if (isSold) color = Colors.red;
+    else if (isSelected) color = Colors.blue;
+    else color = Colors.grey;
+    return GestureDetector(
+      onTap: isSold ? null : () => onTap(seatId),
+      child: Container(
+        margin: EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(4),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          seatId,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+}
