0e4e77109503756904ad62430cfd49ac48be2204
rendraganz <rendrairawan170.com>
2025-12-05 14:27:56 +0700

    revisi validator

--- lib/utils/validators_fahmi.dart
+++ lib/utils/validators_fahmi.dart
@@ -1,5 +1,5 @@
 bool isValidEmailFahmi(String email) {
-  return RegExp(r'^[\w\.-]+@student\.univ\.ac\.id$').hasMatch(email);
+  return RegExp(r'^[\w\.-]+@poliwangi\.ac\.id$').hasMatch(email);
 }
 
 bool isValidPasswordFahmi(String password) {
